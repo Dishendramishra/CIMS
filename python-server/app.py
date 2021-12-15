0c31c3d9328b1467e63449e06f3b60cc6dd55ee5
unknown <dishendramishra@gmail.com>
2021-12-15 15:12:55 +0530

    integrated login

--- python-server/app.py
+++ python-server/app.py
@@ -1,16 +1,31 @@
 from typing import Collection
 from flask import *
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
 from flask_mongoengine import MongoEngine
 import pymongo
 import os
-from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
-from pymongo import response
+from form import LoginForm
+from werkzeug.urls import url_parse
+from colorama import init, Fore, Back, Style
+init()
+
+def cprint(color,obj):
+    colors = {"r": Fore.RED,
+              "b": Fore.BLUE,
+              "g": Fore.GREEN,
+              "w": Fore.WHITE,           
+            }
+    try:
+        print(colors[color],obj,Fore.RESET)
+    except:
+        print(obj)
 
 CIMS_DOMAIN      = os.environ["CIMS_DOMAIN"]
 CIMS_DATABASE    = os.environ["CIMS_DATABASE"]
 CIMS_COLLECTION  = os.environ["CIMS_COLLECTION"]
 CIMS_MONGOUSER   = os.environ["CIMS_MONGOUSER"]
 CIMS_MONGOPASSWD = os.environ["CIMS_MONGOPASSWD"]
+CIMS_MONGOIP     = os.environ["CIMS_MONGOIP"]
 FLASK_SEC_KEY    = os.environ["FLASK_SEC_KEY"]
 
 app = Flask(__name__, template_folder='templates')
@@ -20,17 +35,28 @@ app = Flask(__name__, template_folder='templates')
 # ==================================================================
 app.config['MONGODB_SETTINGS'] = {
     'db'  : CIMS_DATABASE,
-    'host': FLASK_SEC_KEY,
+    'host': CIMS_MONGOIP,
     'port': 27017,
+    'username' : CIMS_MONGOUSER,
+    'password' : CIMS_MONGOPASSWD,
+    'authentication_source': 'admin'
 }
 app.secret_key = FLASK_SEC_KEY
 
-# db = MongoEngine()
-# db.init_app(app)
+db = MongoEngine(app)
 
-# class User(UserMixin,db.Document):
-#     meta = {"collection":"keys"}
-#     key = db.StringField()
+login  = LoginManager(app)
+login.login_view = 'login'
+
+@login.user_loader
+def load_user(user_id):
+    return User.objects(id=user_id).first()
+
+class User(UserMixin, db.Document):
+    meta = {"collection":"users"}
+    username = db.StringField()
+    password = db.StringField()
+    email = db.StringField()
 
 myclient = pymongo.MongoClient(
     "mongodb://{}:27017/".format(CIMS_DOMAIN), 
@@ -50,8 +76,43 @@ def favicon():
                                'prl.png', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/', methods=['GET',"POST"])
-def home():
-    return render_template("view.html")
+@app.route('/login', methods=['GET',"POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("form"))
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        cprint("r",form.username.data)
+        cprint("r",form.password.data)
+        
+        user = User.objects(username=form.username.data,password=form.password.data).first()
+        cprint("r",user)
+
+        if user is None:# or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        
+        login_user(user,remember=form.remember_me.data)
+        cprint("r",form.remember_me.data)
+
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('form')
+            
+        return redirect(next_page)
+    
+    else:
+        return render_template('login.html', form=form)
+
+@app.route('/form', methods=['GET',"POST"])
+def form():
+    return render_template("form.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/add_data', methods=["POST"])
 def add_data():
